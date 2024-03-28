5d7b2295ab9c04f9de4042d0f071428b883a93e6
didier-ds <didiersenou94@gmail.com>
2024-03-29 00:27:26 +0100

    feat: left section ui implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+import defaultTheme from "tailwindcss/defaultTheme";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -5,8 +6,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FA504D",
+        dark: "#2E2F38",
+        "gray-1": "#8E8E8E"
+      },
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+        heading: ["Belgrano", ...defaultTheme.fontFamily.sans]
+      },
+        maxWidth: {
+          "8xl": "98.938rem",
+        },
   },
-  plugins: [],
+    plugins: [],
+  }
 }
 
